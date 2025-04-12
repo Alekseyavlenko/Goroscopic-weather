3abdda5a7cd6059774cb28d709c02e4f5a2307a4
alkseypavlenko <aleksejpavlenko@gmail.com>
2025-04-12 17:44:58 +0300

    git config

--- server.py
+++ server.py
@@ -1,5 +1,25 @@
-h = print('hij')
+from flask import Flask, url_for
+
+app = Flask(__name__)
 
 
-def uhkhmbkhkmjlk():
-    print('llll')
+@app.route('/')
+def index():
+    return f"""<!doctype html>
+                    <html lang="en">
+                      <head>
+                        <meta charset="utf-8">
+                        <title>Goroscopic-weather</title>
+                      </head>
+                      <body>
+                        <h1>Жди нас, Марс!</h1>
+                        <img src="{url_for('static', filename='img/title_image.png')}" 
+                            alt="здесь должна была быть картинка, но не нашлась">
+                        <h4>здесь мы будем прогнозировать погоду</br>
+                        (с гороскопом!)</h4>
+                      </body>
+                    </html>"""
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
