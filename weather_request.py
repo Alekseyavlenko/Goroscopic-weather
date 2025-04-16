45b564b1849e893fa4c6dfbda4fec73e19b20ce7
alkseypavlenko <aleksejpavlenko@gmail.com>
2025-04-16 17:54:29 +0300

    git config

--- weather_request.py
+++ weather_request.py
@@ -3,15 +3,13 @@ import requests
 
 def pogoda_request(sity, country):
     s_city = f"{sity},{country}"
-    city_id = 0
-    appid = "буквенно-цифровой APPID"
     try:
         res = requests.get(
             f"http://api.weatherapi.com/v1/current.json?key=92b3755806724764b2f93911251304&q={s_city}&aqi=no&lang=ru")
         data = res.json()
-        print(data)
+        return data
     except Exception as e:
-        print("Exception (find):", e)
+        return str(f"Exception (find):, {e}")
 
 
 
