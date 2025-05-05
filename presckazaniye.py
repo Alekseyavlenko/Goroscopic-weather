a9de87eebaa210ab121ae8a1151ecaff2b908d8a
katonik193 <katonik12345@gmail.com>
2025-05-05 20:01:28 +0300

    Add files via upload

---
+++ presckazaniye.py
@@ -0,0 +1,14 @@
+import random
+import sqlite3
+
+
+def get_random_prediction(zodiac_sign):
+    conn = sqlite3.connect('goroscope.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT prediction FROM predictions WHERE zodiac_sign = ?', (zodiac_sign,))
+    predictions = cursor.fetchall()
+    conn.close()
+    if predictions:
+        return random.choice(predictions)[0]
+    else:
+        return
